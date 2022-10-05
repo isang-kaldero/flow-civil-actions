9300fe88545f8343929d110f2089d9499da79314
One Cooking Pot <juneevee.ani@gmail.com>
2022-10-05 19:06:01 +0800

    add jquery and jquery-ui

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,92 @@
 $(function() {
 
-    //var xmlDoc = $.parseXML("assets/steps.xml"); 
-    //console.log(xmlDoc);
-
+    var data = [];    
     $.ajax({
 
         type: "GET",
-        url: "./assets/steps.xml",
+        url: "https://isang-kaldero.github.io/flow-civil-actions/assets/steps.xml",
         dataType: "xml",
         success: function(xml){
-            //var xmlDoc = $.parseXML(xml);
-            //var root = $(xmlDoc).find("root");
-            //alert(root);
-            console.log("xml loaded")
+
+
+            xml = $(xml).children();
+            var step_id = "";
+            var first_id = "";
+
+            $(xml).children().each(function (index) {
+                
+                //save data in an array                    
+                step_id = $(this).find("id").text();
+                data[step_id] = [];
+
+                first_id = (index == 0) ? step_id : first_id;
+
+                data[step_id]["title"] = $(this).find("title").text();
+                data[step_id]["content"] = $(this).find("content").text();
+                data[step_id]["yes"] = $(this).find("yes").text();
+                data[step_id]["no"] = $(this).find("no").text();                
+            });                                    
+
+            $(".loader").toggle("fade", 500, function() {
+                populateUI(first_id, ".current", "right");
+            });
+                        
         }
+    });
     
+
+    $(".next").on("click", function(){
+        
+        id = $(this).attr("data-actionid");
+
+        if(id !== "") {
+            populateUI(id, ".standby", "right");
+        }
+
+    });
+
+    $(".back").on("click", function(){
+        
+        id = $(this).attr("data-actionid");
+
+        if(id !== "") {
+            populateUI(id, ".standby", "down");
+        }
+
     });    
 
-/*    $(document).ready(function () {
-        $.ajax({
-          type: "GET",
-          url: "./assets/steps.xml",
-          dataType: "xml",
-          success: xmlParser
-        });
-      });
 
-      function xmlParser(xml) {
 
-        console.log("xml loaded");
-        //$('#load').fadeOut();
-       
-        /*$(xml).find("Book").each(function () {
-          $(".main").append('<div class="book"><div class="title">' + $(this).find("Title").text() + '</div><div class="description">' + $(this).find("Description").text() + '</div><div class="date">Published ' + $(this).find("Date").text() + '</div></div>');
-          $(".book").fadeIn(1000);
+    /**
+     * 
+     * @param {string} id the index from data
+     * @param {string} container_class the class of the div that would be populated
+     * @param {string} direction the slide direction
+     */
+    function populateUI(id, container_class, direction="right") {
+
+        $(container_class + " h1").text(data[id]["title"]);
+        $(container_class + " .step-content").text(data[id]["content"]);
+        
+        $(container_class + " .next").attr("data-actionid", data[id]["yes"]);        
+        $(container_class + " .next").text('Next');
+
+        if(data[id]["no"] == "") {
+            $(container_class + " .back").hide();
+        }        
+        $(container_class + " .back").attr("data-actionid", data[id]["no"]);            
+
+
+        $(container_class).toggle("slide", { direction: direction }, 700, function() {
+
+            if(container_class == ".standby") {
+                $(".main").find(".current").removeClass("current").addClass("standby").hide().css('z-index', 10);
+
+                $(this).removeClass("standby").addClass("current").css('z-index', 1);
+            }
+            
         });
-      }      */
+        
+    }
+    
   });
