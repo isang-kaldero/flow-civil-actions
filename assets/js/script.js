695f98912b4074841cc71490a8c24db337c038a3
One Cooking Pot <juneevee.ani@gmail.com>
2022-10-05 14:32:57 +0800

    Add barebones and css

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(function() {
+
+    $(document).ready(function () {
+        $.ajax({
+          type: "GET",
+          url: "assets/steps.xml",
+          dataType: "xml",
+          success: xmlParser
+        });
+      });
+
+      function xmlParser(xml) {
+
+        console.log("xml loaded");
+        //$('#load').fadeOut();
+       
+        /*$(xml).find("Book").each(function () {
+          $(".main").append('<div class="book"><div class="title">' + $(this).find("Title").text() + '</div><div class="description">' + $(this).find("Description").text() + '</div><div class="date">Published ' + $(this).find("Date").text() + '</div></div>');
+          $(".book").fadeIn(1000);
+        });*/
+      }      
+  });
